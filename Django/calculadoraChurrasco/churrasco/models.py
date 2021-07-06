db3867d1b5107e7eae3a8a166a0a9fe98f99010b
Matheus Brito <matheussouzabrito51@gmail.com>
2021-07-06 16:32:21 -0300

    Criação das models e banco de dados

--- Django/calculadoraChurrasco/churrasco/models.py
+++ Django/calculadoraChurrasco/churrasco/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Produto (models.Model):
+    nome = models.CharField("Nome", max_length=100)
+    
+
+class pedido (models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, blank = True, null = True)
+    quantidade = models.DecimalField("Quantidade",decimal_places=2, max_digits=9)
+    preco = models.DecimalField("Preço médio",decimal_places=2, max_digits=9)
+    cidade = models.CharField("Cidade", max_length=500)
