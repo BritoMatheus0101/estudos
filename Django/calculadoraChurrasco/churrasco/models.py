4b97063fea0c949b54d302f3a238e3ce2ddb6ec9
Matheus Brito <matheussouzabrito51@gmail.com>
2021-07-12 17:40:22 -0300

    Ajuste nos models, criação de nova tabela de relacionamento

--- Django/calculadoraChurrasco/churrasco/models.py
+++ Django/calculadoraChurrasco/churrasco/models.py
@@ -7,11 +7,14 @@ class Produto (models.Model):
     def __str__(self):
         return self.nome
 
+class InformacaoPedido (models.Model):
+   cidade = models.CharField("Cidade", max_length=500)
+   quantidadePessoa = models.DecimalField("Quantidade pessoa", decimal_places=2, max_digits = 9)
+
 class Pedido (models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, blank = True, null = True)
+    lista = models.ForeignKey(InformacaoPedido, on_delete=models.CASCADE, blank = True, null = True)
     quantidade = models.DecimalField("Quantidade",decimal_places=2, max_digits=9)
     preco = models.DecimalField("Preço médio",decimal_places=2, max_digits=9)
-    cidade = models.CharField("Cidade", max_length=500)
-
     
 
