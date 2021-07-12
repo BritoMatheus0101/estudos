fe177f20204b50d647bea7db2054c75fed9f75c4
Matheus Brito <matheussouzabrito51@gmail.com>
2021-07-12 13:25:07 -0300

    Criação de tabelas e login admin

--- Django/calculadoraChurrasco/churrasco/models.py
+++ Django/calculadoraChurrasco/churrasco/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Produto (models.Model):
-    nome = models.CharField("Nome", max_length=100)
+    nome = models.CharField("Nome", max_length=100, unique=True)
     
+    def __str__(self):
+        return self.nome
 
-class pedido (models.Model):
+class Pedido (models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, blank = True, null = True)
     quantidade = models.DecimalField("Quantidade",decimal_places=2, max_digits=9)
     preco = models.DecimalField("Preço médio",decimal_places=2, max_digits=9)
